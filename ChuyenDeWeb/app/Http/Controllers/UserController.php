432b388c278647475e91185d2e18c1398a8cb754
Bùi Anh Tú <tubuianh2017@gmail.com>
2024-10-26 22:05:59 +0700

    Add function update role.

--- ChuyenDeWeb/app/Http/Controllers/UserController.php
+++ ChuyenDeWeb/app/Http/Controllers/UserController.php
@@ -10,6 +10,7 @@ use App\Rules\SingleSpaceOnly;
 use App\Rules\GmailOnly;
 use App\Rules\NoSpace;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
     /**
@@ -187,5 +188,61 @@ class UserController extends Controller
         } catch (\Exception $e) {
             return redirect()->route('userAdmin.index')->with('error', 'Xóa người dùng không thành công');
         }
+    }
+
+    public function listRole()
+    {
+        $users = User::paginate(5);
+        return view('adminPage', compact('users'));
+    }
+    
+    public function updatePermissions(Request $request, $id)
+    {
+        $roleHierarchy = [
+            'user' => 1,
+            'editor' => 2,
+            'admin' => 3,
+        ];
+
+        $defaultPermissions = [
+            'user' => 'viewer',
+            'editor' => 'editor',
+            'admin' => 'full_access',
+        ];
+
+        $request->validate([
+            'role' => 'required|in:user,editor,admin',
+        ],[
+            'role.required' => 'Vui lòng chọn vai trò người dùng',
+            'role.in' => 'Vai trò không hợp lệ',
+        ]);
+
+        $user = User::findOrFail($id);
+        if (!$user) {
+            return response()->json(['success' => false, 'message' => 'Người dùng không tồn tại']);
+        }
+
+        $currentUserRole = Auth::user()->role;
+        $currentUserRoleLevel = $roleHierarchy[$currentUserRole];
+        $targetUserRoleLevel = $roleHierarchy[$user->role];
+        $newRoleLevel = $roleHierarchy[$request->input('role')];
+
+        if ($currentUserRole == 'admin') {
+            $user->role = $request->input('role');
+            $user->permission = $defaultPermissions[$user->role];
+        } elseif ($currentUserRole == 'editor') {
+            if ($newRoleLevel <= $targetUserRoleLevel + 1 && $newRoleLevel <= $currentUserRoleLevel) {
+                $user->role = $request->input('role');
+                $user->permission = $defaultPermissions[$user->role];
+            } else {
+                return response()->json(['success' => false, 'message' => 'Bạn không thể thay đổi quyền của người dùng có cấp độ quyền cao hơn bạn.']);
+            }
+        } else {
+            return response()->json(['success' => false, 'message' => 'Bạn không có quyền cập nhật quyền hạn của người dùng.']);
+        }
+
+        $user->save();
+
+        return response()->json(['success' => true, 'message' => 'Cập nhật quyền truy cập thành công']);
     }
 }
